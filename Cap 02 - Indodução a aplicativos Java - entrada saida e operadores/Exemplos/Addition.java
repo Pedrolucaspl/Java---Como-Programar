288e872b169eff1b7a05963193367e898e74df03
Pedrolucasfb <pedrolucasf.pl@gmail.com>
2026-05-25 23:18:59 -0300

    Adição de Exemplos do Cap 02

---
+++ Cap 02 - Indodução a aplicativos Java - entrada saida e operadores/Exemplos/Addition.java
@@ -0,0 +1,24 @@
+//Programa de adição que insere dois numeros e exibe a soma
+import java.util.Scanner; //importa a classe Scanner
+
+public class Addition {
+    //metodo main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+        //criar um scanner para obter entrada a partir da janela de comando
+        Scanner input = new Scanner(System.in);
+
+        int number1; //primeiro numero a ser adicionado
+        int number2; //segundo numero a ser adicionado
+        int sum; //variavel para armazenar a soma dos numeros
+
+        System.out.print("Enter first integer: "); //prompt
+        number1 = input.nextInt(); //le o primeiro numero do usuario
+
+        System.out.print("Enter second integer: "); //prompt
+        number2 = input.nextInt(); //le o segundo numero do usuario
+
+        sum = number1 + number2; //soma os numeros, depois armazena o resultado em sum
+
+        System.out.printf("Sum is %d%n", sum); //exibe a soma
+    }//fim do metodo main
+}//fim da classe Addition
---
+++ Cap 02 - Indodução a aplicativos Java - entrada saida e operadores/Exemplos/Welcome1.java
@@ -0,0 +1,10 @@
+//Programa de impressão de texto
+
+public class Welcome1
+{
+    //metodo main inicia a execução do aplicativo Java
+   public static void main(String[] args)
+   {
+      System.out.println("Welcome to Java Programming!");
+   }//fim do metodo main
+}//fim da classe Welcome1
